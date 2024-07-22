63f8ef400935d455646668d6f449b85f30ea6f3e
hartum <hartum@gmail.com>
2024-07-22 23:46:47 +0200

    Mejoras visuales

--- src/components/TravelItem.vue
+++ src/components/TravelItem.vue
@@ -3,16 +3,19 @@
 		<IonItem class="item-travel" button="true" @click="editTravel">
 			<IonLabel>
 				<IonIcon
-					:icon="calendarClearOutline"
-					class="icon-calendar"
-					size="large"
-				></IonIcon>
-				<IonIcon
 					:icon="payIcons[travel.payMethod]"
 					size="default"
 					class="icon-travel"
 				></IonIcon>
-				<span>{{ formatDate(travel.startDate) }}</span>
+
+				<div class="calendar">
+					<em></em>
+					<strong>{{ month(travel.startDate) }}</strong>
+					<span>{{ day(travel.startDate) }}</span>
+				</div>
+				<div class="time">
+					{{ hour(travel.startDate) }}
+				</div>
 
 				<span class="money income">
 					<b>{{ travel.amount }}{{ currency }} </b>
@@ -44,6 +47,7 @@
 		cashOutline,
 		cardOutline,
 		calendarClearOutline,
+		carSport,
 	} from 'ionicons/icons';
 
 	const props = defineProps({
@@ -76,6 +80,18 @@
 	const formatDate = (date) => {
 		return moment(date).format('DD MMM - HH:mm');
 	};
+	const day = (date) => {
+		return moment(date).format('DD');
+	};
+	const weekday = (date) => {
+		return moment(date).format('dddd');
+	};
+	const hour = (date) => {
+		return moment(date).format('HH:mm');
+	};
+	const month = (date) => {
+		return moment(date).format('MMM');
+	};
 </script>
 
 <style scoped lang="scss">
@@ -89,28 +105,104 @@
 	}
 
 	.money {
-		font-size: 1.4em;
+		font-size: 2.4em;
 		vertical-align: text-bottom;
 		float: right;
 		color: #666;
 	}
 
 	.icon-travel {
+		/*
 		vertical-align: middle;
-		color: #4b4a4a;
-		width: 1.5em;
-		height: 1.5em;
+		
+		
 		float: right;
 		margin-left: 5px;
 		margin-top: 3px;
+		*/
+		width: 2em;
+		height: 2em;
+		color: #949494;
+		float: right;
+		/*position: absolute;*/
+		right: 20px;
+		transform: translateY(25%) translateX(25%);
 	}
 	.icon-calendar {
-		vertical-align: sub;
-		color: #949494;
+		/*
+		vertical-align: middle;
 		margin-right: 5px;
+		margin-top: -3px;
+		*/
 	}
 
 	.income {
 		color: #087702;
+	}
+
+	div.calendar {
+		float: left;
+		font-size: 0.4em; /* change icon size */
+		display: block;
+		position: relative;
+		width: 44px;
+		height: 45px;
+		background-color: #fff;
+		border-radius: 0.6em;
+		box-shadow: 0 1px 0 #bdbdbd, 0 2px 0 #fff, 0 3px 0 #bdbdbd, 0 4px 0 #fff,
+			0 5px 0 #bdbdbd, 0 0 0 1px #bdbdbd;
+		overflow: hidden;
+	}
+
+	div.calendar * {
+		display: block;
+		width: 100%;
+		font-size: 2em;
+		font-weight: bold;
+		font-style: normal;
+		text-align: center;
+	}
+
+	div.calendar strong {
+		font-size: 1.5em;
+		position: absolute;
+		top: 0;
+		text-transform: uppercase;
+		padding: 0.2em 0;
+		color: #fff;
+		background-color: #696868;
+		border-bottom: 1px dashed #fff;
+		box-shadow: 0 2px 0 #696868;
+	}
+
+	div.calendar em {
+		position: absolute;
+		bottom: 0.3em;
+		color: #2f2f2f;
+	}
+
+	div.calendar span {
+		width: 100%;
+		font-size: 2.8em;
+		letter-spacing: -0.05em;
+		padding-top: 1.1em;
+		color: #2f2f2f;
+	}
+	div.time {
+		float: left;
+		font-size: 1.5em; /* change icon size */
+		font-weight: bold;
+		display: block;
+		position: relative;
+		padding-top: 0.35em;
+		margin-left: 0.35em;
+		width: 80px;
+		height: 45px;
+		text-align: center;
+		background-color: #fff;
+		border-radius: 0.2em;
+		box-shadow: 0 1px 0 #bdbdbd, 0 2px 0 #fff, 0 3px 0 #bdbdbd, 0 4px 0 #fff,
+			0 5px 0 #bdbdbd, 0 0 0 1px #bdbdbd;
+		overflow: hidden;
 	}
 </style>
